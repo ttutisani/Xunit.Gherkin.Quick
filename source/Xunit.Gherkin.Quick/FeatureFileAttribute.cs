8c0dede8c49012bf50f1d05244e5827d846580d2
Christian Pejrup <github.christian@pejrup.com>
2018-04-01 12:17:48 +0200

    Refactored the Feature class
    - One file per class
    - Changed exception types from base exception to more specific types

---
+++ source/Xunit.Gherkin.Quick/FeatureFileAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Xunit.Gherkin.Quick
+{
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public sealed class FeatureFileAttribute : Attribute
+    {
+        /// <summary>
+        /// Specify path to feature file (can be relative to output folder).
+        /// </summary>
+        /// <param name="path">
+        /// Path to feature file. Can be relative to output folder,
+        /// e.g. "./SampleFolder/Sample.feature"
+        /// </param>
+        public FeatureFileAttribute(string path)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+        }
+
+        public string Path { get; }
+    }
+}
