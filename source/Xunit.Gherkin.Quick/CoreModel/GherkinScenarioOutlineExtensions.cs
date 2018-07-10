3c39f8c68f0a76aeb69a62f8adc0ac6f53fe0d93
Tengiz <tengo_tutisani@hotmail.com>
2018-07-09 20:53:14 -0500

    Split GherkinScenarioOutlineExtensions into methods (extract method refactoring).

--- source/Xunit.Gherkin.Quick/CoreModel/GherkinScenarioOutlineExtensions.cs
+++ source/Xunit.Gherkin.Quick/CoreModel/GherkinScenarioOutlineExtensions.cs
@@ -14,46 +14,23 @@ namespace Xunit.Gherkin.Quick
             string exampleName,
             int exampleRowIndex)
         {
-            var example = @this.Examples.FirstOrDefault(e => e.Name == exampleName);
-            if (example == null)
-                throw new InvalidOperationException($"Cannot find example named `{exampleName}` in scenario outline `{@this.Name}`.");
+            var examples = @this.Examples.FirstOrDefault(e => e.Name == exampleName);
+            if (examples == null)
+                throw new InvalidOperationException($"Cannot find examples named `{exampleName}` in scenario outline `{@this.Name}`.");
 
-            var exampleRows = example.TableBody.ToList();
-            if (!(exampleRowIndex < exampleRows.Count))
-                throw new InvalidOperationException($"Index out of range. Cannot retrieve example row at index `{exampleRowIndex}`. Example: `{example.Name}`. Scenario outline: `{@this.Name}`.");
-
-            var exampleRow = exampleRows[exampleRowIndex];
-            var rowCells = exampleRow.Cells.ToList();
-
-            var rowValues = new Dictionary<string, string>();
-
-            var headerCells = example.TableHeader.Cells.ToList();
-            for (int index = 0; index < headerCells.Count; index++)
-            {
-                rowValues.Add(headerCells[index].Value, rowCells[index].Value);
-            }
+            var exampleRow = GetExampleRow(@this, exampleRowIndex, examples);
+            var rowValues = GetExampleRowValues(examples, exampleRow);
 
             var scenarioSteps = new List<global::Gherkin.Ast.Step>();
 
             foreach (var outlineStep in @this.Steps)
             {
-                var stepText = _placeholderRegex.Replace(outlineStep.Text, 
-                    match => 
-                    {
-                        var placeholderKey = match.Groups[1].Value;
-                        if (!rowValues.ContainsKey(placeholderKey))
-                            throw new InvalidOperationException($"Examples table did not provide value for `{placeholderKey}`. Scenario outline: `{@this.Name}`. Examples: `{exampleName}`. Row index: {exampleRowIndex}.");
-
-                        var placeholderValue = rowValues[placeholderKey];
-
-                        return placeholderValue;
-                    });
-
-                var scenarioStep = new global::Gherkin.Ast.Step(
-                    outlineStep.Location,
-                    outlineStep.Keyword,
-                    stepText,
-                    outlineStep.Argument);
+                var scenarioStep = DigestExampleValuesIntoStep(
+                    @this, 
+                    exampleName, 
+                    exampleRowIndex, 
+                    rowValues, 
+                    outlineStep);
 
                 scenarioSteps.Add(scenarioStep);
             }
@@ -65,6 +42,52 @@ namespace Xunit.Gherkin.Quick
                 @this.Name,
                 @this.Description,
                 scenarioSteps.ToArray());
+        }
+
+        private static global::Gherkin.Ast.Step DigestExampleValuesIntoStep(global::Gherkin.Ast.ScenarioOutline @this, string exampleName, int exampleRowIndex, Dictionary<string, string> rowValues, global::Gherkin.Ast.Step outlineStep)
+        {
+            var stepText = _placeholderRegex.Replace(outlineStep.Text,
+                                match =>
+                                {
+                                    var placeholderKey = match.Groups[1].Value;
+                                    if (!rowValues.ContainsKey(placeholderKey))
+                                        throw new InvalidOperationException($"Examples table did not provide value for `{placeholderKey}`. Scenario outline: `{@this.Name}`. Examples: `{exampleName}`. Row index: {exampleRowIndex}.");
+
+                                    var placeholderValue = rowValues[placeholderKey];
+
+                                    return placeholderValue;
+                                });
+
+            var scenarioStep = new global::Gherkin.Ast.Step(
+                outlineStep.Location,
+                outlineStep.Keyword,
+                stepText,
+                outlineStep.Argument);
+            return scenarioStep;
+        }
+
+        private static Dictionary<string, string> GetExampleRowValues(global::Gherkin.Ast.Examples examples, List<global::Gherkin.Ast.TableCell> exampleRowCells)
+        {
+            var rowValues = new Dictionary<string, string>();
+
+            var headerCells = examples.TableHeader.Cells.ToList();
+            for (int index = 0; index < headerCells.Count; index++)
+            {
+                rowValues.Add(headerCells[index].Value, exampleRowCells[index].Value);
+            }
+
+            return rowValues;
+        }
+
+        private static List<global::Gherkin.Ast.TableCell> GetExampleRow(global::Gherkin.Ast.ScenarioOutline @this, int exampleRowIndex, global::Gherkin.Ast.Examples examples)
+        {
+            var exampleRows = examples.TableBody.ToList();
+            if (!(exampleRowIndex < exampleRows.Count))
+                throw new InvalidOperationException($"Index out of range. Cannot retrieve example row at index `{exampleRowIndex}`. Example: `{examples.Name}`. Scenario outline: `{@this.Name}`.");
+
+            var exampleRow = exampleRows[exampleRowIndex];
+            var exampleRowCells = exampleRow.Cells.ToList();
+            return exampleRowCells;
         }
     }
 }
