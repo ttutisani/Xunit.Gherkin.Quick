989923d55cc33e7032729902560b1430d666753e
Tengiz <tengo_tutisani@hotmail.com>
2018-09-25 21:17:29 -0500

    Scenario background usage sample test.

--- source/Xunit.Gherkin.Quick.ProjectConsumer/Addition/AddNumbersTo5.cs
+++ source/Xunit.Gherkin.Quick.ProjectConsumer/Addition/AddNumbersTo5.cs
@@ -1,35 +1,30 @@
 ﻿namespace Xunit.Gherkin.Quick.ProjectConsumer.Addition
 {
-    [FeatureFile("./Addition/AddTwoNumbers.feature")]
-    public sealed partial class AddTwoNumbers : Feature
+    [FeatureFile("./Addition/AddNumbersTo5.feature")]
+    public sealed class AddNumbersTo5 : Feature
     {
         private readonly Calculator _calculator = new Calculator();
 
         [Given(@"I chose (-?\d+) as first number")]
-        [When(@"I choose (-?\d+) as first number")]
-        public void I_chose_first_number(int firstNumber)
+        public void Given_I_chose_x_as_first_number(int firstNumber)
         {
             _calculator.SetFirstNumber(firstNumber);
         }
 
         [And(@"I chose (-?\d+) as second number")]
-        [And(@"I choose (-?\d+) as second number")]
-        public void I_chose_second_number(int secondNumber)
+        public void And_I_chose_y_as_second_number(int secondNumber)
         {
             _calculator.SetSecondNumber(secondNumber);
         }
 
         [When(@"I press add")]
-        [And(@"I pressed add")]
-        [And(@"I press add")]
-        public void I_press_add()
+        public void When_I_press_add()
         {
             _calculator.AddNumbers();
         }
 
         [Then(@"the result should be (-?\d+) on the screen")]
-        [And(@"I saw (-?\d+) on the screen")]
-        public void The_result_should_be_z_on_the_screen(int expectedResult)
+        public void Then_the_result_should_be_z_on_the_screen(int expectedResult)
         {
             var actualResult = _calculator.Result;
 
---
+++ source/Xunit.Gherkin.Quick.ProjectConsumer/Addition/AddNumbersTo5.feature
@@ -0,0 +1,32 @@
+﻿Feature: Adding numbers to 5
+	As a fanatic of "5"
+	I want to add other numbers to it
+	So that I can see the whole power of "5"
+
+Background:
+	Given I chose 5 as first number
+
+Scenario: Add 1 to 5
+	And I chose 1 as second number
+	When I press add
+	Then the result should be 6 on the screen
+
+Scenario: Add 5 to 5
+	And I chose 5 as second number
+	When I press add
+	Then the result should be 10 on the screen
+
+Scenario Outline: Add various numbers to 5
+	And I chose <b> as second number
+	When I press add
+	Then the result should be <c> on the screen
+
+	Examples:
+		| b | c  |
+		| 1 | 6  |
+		| 5 | 10 |
+
+	Examples: of negative numbers
+		| b  | c |
+		| -1 | 4 |
+		| -5 | 0 |
