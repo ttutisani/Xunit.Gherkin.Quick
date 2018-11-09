7bba112d5a6b38a8585325b237eba9d733038568
Tengiz Tutisani <ttutisani@users.noreply.github.com>
2018-11-08 20:36:19 -0600

    Before and after scenario execution hook docs. (#67)
    * BDD test to log before/after hook messages.
    
    * Instructions of using scenario before/after hooks.
    
    * Improved sample test to clarify the scenario before/after hooks.
    
    * Link to scenario before-after hooks doc.
    
    * Improved wording.
    
    * Clarified use of ITestOutput Helper.

---
+++ source/Xunit.Gherkin.Quick.ProjectConsumer/BeforeAfterHooks/BeforeAfter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit.Abstractions;
+
+namespace Xunit.Gherkin.Quick.ProjectConsumer.BeforeAfterHooks
+{
+    [FeatureFile("./BeforeAfterHooks/BeforeAfter.feature")]
+    public sealed class BeforeAfter : Feature, IDisposable
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public BeforeAfter(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+
+            _testOutputHelper.WriteLine("Before executing scenario.");
+        }
+
+        [Given("first step executed")]
+        public void First_Step_Executed()
+        {
+            _testOutputHelper.WriteLine("First step.");
+        }
+
+        [And("second step executed")]
+        public void Second_Step_Executed()
+        {
+            _testOutputHelper.WriteLine("Second step.");
+        }
+
+        public void Dispose()
+        {
+            _testOutputHelper.WriteLine("After scenario execution.");
+        }
+    }
+}
---
+++ source/Xunit.Gherkin.Quick.ProjectConsumer/BeforeAfterHooks/BeforeAfter.feature
@@ -0,0 +1,8 @@
+﻿Feature: Before/After Hooks
+	As a BDD developer using Xunit.Gherkin.Quick
+	I want to ensure that Before and After hooks work for Scenario
+	So that I can write code that executes either before or after the Scenario
+
+Scenario: Scenario Before and After
+	Given first step executed
+	And second step executed
