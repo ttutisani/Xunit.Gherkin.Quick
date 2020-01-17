9e94aab91150a99648a367987b97fce8b9a03534
Tengiz Tutisani <tengo_tutisani@hotmail.com>
2020-01-17 13:40:27 -0500

    Removed commented line.

--- source/Xunit.Gherkin.Quick/ScenarioXunitHook/Scenario/ScenarioDiscoverer.cs
+++ source/Xunit.Gherkin.Quick/ScenarioXunitHook/Scenario/ScenarioDiscoverer.cs
@@ -24,7 +24,6 @@ namespace Xunit.Gherkin.Quick
             ITestMethod testMethod, 
             IAttributeInfo factAttribute)
         {
-            //var feature = GetGherkinDocumentByType(testMethod.TestClass.Class.ToRuntimeType()).Feature;
             var feature = new FeatureDiscoveryModel(new FeatureFileRepository()).Discover(testMethod.TestClass.Class.ToRuntimeType());
 
             foreach (var scenario in feature.Children.OfType<global::Gherkin.Ast.Scenario>())
