f005f1c56fd6f5ed7c9183f59fffd88d381222e9
Miguel <mcarboni@afip.gob.ar>
2022-09-09 11:44:16 -0300

    tests class info

---
+++ source/Xunit.Gherkin.Quick.UnitTests/FeatureClassInfo_FromFeatureClassType_Should.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace UnitTests
+{
+    public sealed class FeatureClassInfo_FromFeatureClassType_Should
+    {
+        [Fact]
+        public void Require_ClassType()
+        {
+            //act / assert.
+            Assert.Throws<ArgumentNullException>(() => MissingFeatureClassInfo.FromMissingFeatureClassType(null));
+        }
+
+        [Theory]
+        [InlineData(typeof(MyFeature), "MyFeature*.feature")]
+        [InlineData(typeof(MyFeatureWithPattern), "someother.pattern")]
+        [InlineData(typeof(MyFeatureWithFileNameAndPattern), "someother.pattern")]
+        public void Construct_InfoClass_With_Search_Pattern(
+            Type classType,
+            string pattern)
+        {
+            //act.
+            var sut = FeatureClassInfo.FromFeatureClassType(classType);
+
+            //assert.
+            Assert.NotNull(sut);
+            Assert.Equal(pattern, sut.FileNameSearchPattern);
+        }
+
+        private sealed class MyFeature : Feature { }
+
+        [FeatureFileSearchPattern("someother.pattern")]
+        private sealed class MyFeatureWithPattern : Feature { }
+
+        [FeatureFile("some.file")]
+        [FeatureFileSearchPattern("someother.pattern")]
+        private sealed class MyFeatureWithFileNameAndPattern : Feature { }
+    }
+}
