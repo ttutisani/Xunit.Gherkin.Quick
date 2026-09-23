1a13283748c17f27ce1bbdcf2899d3bbe488a50b
Andrei <andrei_fangli@outlook.com>
2026-09-23 18:57:06 +0300

    Added Multi-Language Support (#163)
    * Added multi-language support
    
    * Moved translations to FeatureFile
    
    * Cleanup
    
    * Fixed typos

--- source/Xunit.Gherkin.Quick.UnitTests/FeatureFileTests.cs
+++ source/Xunit.Gherkin.Quick.UnitTests/FeatureFileTests.cs
@@ -48,6 +48,42 @@ namespace UnitTests
         }
 
         [Fact]
+        public void GetScenario_Applies_Translation()
+        {
+            //arrange.
+            var sut = new FeatureFile(CreateGherkinDocumentWithSlovakScenario("existing"));
+
+            //act.
+            var scenario = sut.GetScenario("existing");
+
+            //assert.
+            Assert.NotNull(scenario);
+            Assert.Collection(
+                scenario.Steps,
+                firstStep => Assert.Multiple(
+                    () => Assert.Equal("Given ", firstStep.Keyword),
+                    () => Assert.Equal("given", firstStep.Text)
+                ),
+                secondStep => Assert.Multiple(
+                    () => Assert.Equal("And ", secondStep.Keyword),
+                    () => Assert.Equal("and", secondStep.Text)
+                ),
+                thirdStep => Assert.Multiple(
+                    () => Assert.Equal("But ", thirdStep.Keyword),
+                    () => Assert.Equal("but", thirdStep.Text)
+                ),
+                fourthStep => Assert.Multiple(
+                    () => Assert.Equal("When ", fourthStep.Keyword),
+                    () => Assert.Equal("when", fourthStep.Text)
+                ),
+                fifthStep => Assert.Multiple(
+                    () => Assert.Equal("Then ", fifthStep.Keyword),
+                    () => Assert.Equal("then", fifthStep.Text)
+                )
+            );
+        }
+
+        [Fact]
         public void GetScenarioOutline_Retrieves_If_Found()
         {
             //arrange.
@@ -76,6 +112,42 @@ namespace UnitTests
         }
 
         [Fact]
+        public void GetScenarioOutline_Applies_Translation()
+        {
+            //arrange.
+            var sut = new FeatureFile(CreateGherkinDocumentWithSlovakScenarioOutline("existing"));
+
+            //act.
+            var scenarioOutline = sut.GetScenarioOutline("existing");
+
+            //assert.
+            Assert.NotNull(scenarioOutline);
+            Assert.Collection(
+                scenarioOutline.Steps,
+                firstStep => Assert.Multiple(
+                    () => Assert.Equal("Given ", firstStep.Keyword),
+                    () => Assert.Equal("given", firstStep.Text)
+                ),
+                secondStep => Assert.Multiple(
+                    () => Assert.Equal("And ", secondStep.Keyword),
+                    () => Assert.Equal("and", secondStep.Text)
+                ),
+                thirdStep => Assert.Multiple(
+                    () => Assert.Equal("But ", thirdStep.Keyword),
+                    () => Assert.Equal("but", thirdStep.Text)
+                ),
+                fourthStep => Assert.Multiple(
+                    () => Assert.Equal("When ", fourthStep.Keyword),
+                    () => Assert.Equal("when", fourthStep.Text)
+                ),
+                fifthStep => Assert.Multiple(
+                    () => Assert.Equal("Then ", fifthStep.Keyword),
+                    () => Assert.Equal("then", fifthStep.Text)
+                )
+            );
+        }
+
+        [Fact]
         public void GetBackground_Retrieves_If_Present()
         {
             var sut = new FeatureFile(CreateGherkinDocumentWithBackground());
@@ -89,6 +161,42 @@ namespace UnitTests
             var sut = new FeatureFile(CreateGherkinDocumentWithScenario("test"));
             var background = sut.GetBackground();
             Assert.Null(background);
+        }
+
+        [Fact]
+        public void GetBackground_Applies_Translation()
+        {
+            //arrange.
+            var sut = new FeatureFile(CreateGherkinDocumentWithSlovakBackground());
+
+            //act.
+            var background = sut.GetBackground();
+
+            //assert.
+            Assert.NotNull(background);
+            Assert.Collection(
+                background.Steps,
+                firstStep => Assert.Multiple(
+                    () => Assert.Equal("Given ", firstStep.Keyword),
+                    () => Assert.Equal("given", firstStep.Text)
+                ),
+                secondStep => Assert.Multiple(
+                    () => Assert.Equal("And ", secondStep.Keyword),
+                    () => Assert.Equal("and", secondStep.Text)
+                ),
+                thirdStep => Assert.Multiple(
+                    () => Assert.Equal("But ", thirdStep.Keyword),
+                    () => Assert.Equal("but", thirdStep.Text)
+                ),
+                fourthStep => Assert.Multiple(
+                    () => Assert.Equal("When ", fourthStep.Keyword),
+                    () => Assert.Equal("when", fourthStep.Text)
+                ),
+                fifthStep => Assert.Multiple(
+                    () => Assert.Equal("Then ", fifthStep.Keyword),
+                    () => Assert.Equal("then", fifthStep.Text)
+                )
+            );
         }
 
         private static Gherkin.Ast.GherkinDocument CreateGherkinDocumentWithScenario(
@@ -142,5 +250,66 @@ namespace UnitTests
                 }),
                 new Gherkin.Ast.Comment[0]);
         }
+
+        private static Gherkin.Ast.GherkinDocument CreateGherkinDocumentWithSlovakScenario(string scenario)
+        {
+            return new Gherkin.Ast.GherkinDocument(
+                new Gherkin.Ast.Feature(new Gherkin.Ast.Tag[0], null, "sk", null, null, null, new Gherkin.Ast.ScenarioDefinition[]
+                {
+                    new Gherkin.Ast.Scenario(
+                        new Gherkin.Ast.Tag[0],
+                        null,
+                        null,
+                        scenario,
+                        null,
+                        CreateSlovakSteps()
+                    )
+                }),
+                new Gherkin.Ast.Comment[0]);
+        }
+
+        private static Gherkin.Ast.GherkinDocument CreateGherkinDocumentWithSlovakScenarioOutline(string scenario)
+        {
+            return new Gherkin.Ast.GherkinDocument(
+                new Gherkin.Ast.Feature(new Gherkin.Ast.Tag[0], null, "sk", null, null, null, new Gherkin.Ast.ScenarioDefinition[]
+                {
+                    new Gherkin.Ast.ScenarioOutline(
+                        new Gherkin.Ast.Tag[0],
+                        null,
+                        null,
+                        scenario,
+                        null,
+                        CreateSlovakSteps(),
+                        new Gherkin.Ast.Examples[0]
+                    )
+                }),
+                new Gherkin.Ast.Comment[0]);
+        }
+
+        private static Gherkin.Ast.GherkinDocument CreateGherkinDocumentWithSlovakBackground()
+        {
+            return new Gherkin.Ast.GherkinDocument(
+                new Gherkin.Ast.Feature(new Gherkin.Ast.Tag[0], null, "sk", null, null, null, new Gherkin.Ast.ScenarioDefinition[]
+                {
+                    new Gherkin.Ast.Background(
+                        null,
+                        null,
+                        "background",
+                        null,
+                        CreateSlovakSteps()
+                    )
+                }),
+                new Gherkin.Ast.Comment[0]);
+        }
+
+        private static Gherkin.Ast.Step[] CreateSlovakSteps()
+            => new Gherkin.Ast.Step[]
+            {
+                new Gherkin.Ast.Step(null, "Pokiaľ ", "given", null),
+                new Gherkin.Ast.Step(null, "A ", "and", null),
+                new Gherkin.Ast.Step(null, "Ale ", "but", null),
+                new Gherkin.Ast.Step(null, "Keď ", "when", null),
+                new Gherkin.Ast.Step(null, "Tak ", "then", null)    
+            };
     }
 }
--- source/Xunit.Gherkin.Quick/CoreModel/FeatureFile/FeatureFile.cs
+++ source/Xunit.Gherkin.Quick/CoreModel/FeatureFile/FeatureFile.cs
@@ -1,4 +1,7 @@
-﻿using Gherkin.Ast;
+﻿using Gherkin;
+using Gherkin.Ast;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Xunit.Gherkin.Quick
@@ -8,23 +11,89 @@ namespace Xunit.Gherkin.Quick
         public GherkinDocument GherkinDocument { get; }
 
         public FeatureFile(GherkinDocument gherkinDocument)
-        {
-            GherkinDocument = gherkinDocument ?? throw new System.ArgumentNullException(nameof(gherkinDocument));
-        }
+            => GherkinDocument = gherkinDocument ?? throw new ArgumentNullException(nameof(gherkinDocument));
 
         public global::Gherkin.Ast.Scenario GetScenario(string scenarioName)
         {
-            return GherkinDocument.Feature.Children.FirstOrDefault(s => s.Name == scenarioName) as global::Gherkin.Ast.Scenario;
+            var scenario = GherkinDocument.Feature.Children.FirstOrDefault(s => s.Name == scenarioName) as global::Gherkin.Ast.Scenario;
+            if (scenario is null || string.IsNullOrWhiteSpace(GherkinDocument.Feature.Language))
+                return scenario;
+
+            var dialectProvider = new GherkinDialectProvider();
+            var gherkinDialect = dialectProvider.GetDialect(GherkinDocument.Feature.Language, GherkinDocument.Feature.Location);
+
+            return new global::Gherkin.Ast.Scenario(
+                (scenario.Tags as Tag[])?.ToArray(),
+                scenario.Location,
+                scenario.Keyword,
+                scenario.Name,
+                scenario.Description,
+                _Translate(scenario.Steps, gherkinDialect)
+            );
         }
 
-		public global::Gherkin.Ast.Background GetBackground()
-		{
-			return GherkinDocument.Feature.Children.OfType<global::Gherkin.Ast.Background>().SingleOrDefault();
-		}
+        public Background GetBackground()
+        {
+            var background = GherkinDocument.Feature.Children.OfType<Background>().SingleOrDefault();
+            if (background is null || string.IsNullOrWhiteSpace(GherkinDocument.Feature.Language))
+                return background;
+
+            var dialectProvider = new GherkinDialectProvider();
+            var gherkinDialect = dialectProvider.GetDialect(GherkinDocument.Feature.Language, GherkinDocument.Feature.Location);
+
+            return new Background(
+                background.Location,
+                background.Keyword,
+                background.Name,
+                background.Description,
+                _Translate(background.Steps, gherkinDialect)
+            );
+        }
 
         internal ScenarioOutline GetScenarioOutline(string scenarioOutlineName)
         {
-            return GherkinDocument.Feature.Children.FirstOrDefault(s => s.Name == scenarioOutlineName) as global::Gherkin.Ast.ScenarioOutline;
+            var scenarioOutline = GherkinDocument.Feature.Children.FirstOrDefault(s => s.Name == scenarioOutlineName) as ScenarioOutline;
+            if (scenarioOutline is null || string.IsNullOrWhiteSpace(GherkinDocument.Feature.Language))
+                return scenarioOutline;
+
+            var dialectProvider = new GherkinDialectProvider();
+            var gherkinDialect = dialectProvider.GetDialect(GherkinDocument.Feature.Language, GherkinDocument.Feature.Location);
+
+            return new ScenarioOutline(
+                (scenarioOutline.Tags as Tag[])?.ToArray(),
+                scenarioOutline.Location,
+                scenarioOutline.Keyword,
+                scenarioOutline.Name,
+                scenarioOutline.Description,
+                _Translate(scenarioOutline.Steps, gherkinDialect),
+                (scenarioOutline.Examples as Examples[])?.ToArray()
+            );
+        }
+
+        private Step[] _Translate(IEnumerable<Step> steps, GherkinDialect gherkinDialect)
+            => steps
+                .Select(step => _TranslateStep(step, gherkinDialect))
+                .ToArray();
+
+        private static Step _TranslateStep(Step step, GherkinDialect gherkinDialect)
+        {
+            string translatedKeyword = null;
+            if (step.Keyword.Trim() != "*")
+                if (gherkinDialect.GivenStepKeywords.Contains(step.Keyword))
+                    translatedKeyword = "Given ";
+                else if (gherkinDialect.WhenStepKeywords.Contains(step.Keyword))
+                    translatedKeyword = "When ";
+                else if (gherkinDialect.ThenStepKeywords.Contains(step.Keyword))
+                    translatedKeyword = "Then ";
+                else if (gherkinDialect.AndStepKeywords.Contains(step.Keyword))
+                    translatedKeyword = "And ";
+                else if (gherkinDialect.ButStepKeywords.Contains(step.Keyword))
+                    translatedKeyword = "But ";
+
+            if (translatedKeyword != null)
+                return new Step(step.Location, translatedKeyword, step.Text, step.Argument);
+            else
+                return step;
         }
     }
 }
